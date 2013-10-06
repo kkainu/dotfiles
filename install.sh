84b6c9fffba40d51b853f404bb0292f65f3a2b2c
Kalle Kainu <kkainu@gmail.com>
2013-10-06 11:36:30 +0300

    delete old dotfiles instead of moving

--- install.sh
+++ install.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
 dir=~/.dotfiles
-olddir=~/.dotfiles_old
-
-mkdir -p $olddir
 cd $dir
 
 for file in $(find .  ! -name . -maxdepth 1 ! -name install.sh ! -name .git ! -name README.md); do
     dotfile=$(basename $file)
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$dotfile $olddir/
+    rm ~/.$dotfile
     echo "Creating symlink to $dotfile in home directory."
     ln -s $dir/$dotfile ~/.$dotfile
 done
